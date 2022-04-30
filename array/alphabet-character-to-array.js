3320d13fbc729eda19f6902f17bd287cccb743bb
Saifur Rahman <saifur703@gmail.com>
2022-04-30 22:06:15 +0600

    Alphabet character to Array Convert

---
+++ array/alphabet-character-to-array.js
@@ -0,0 +1,14 @@
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+console.log(alphabet.split(""));
+
+// OR,
+console.log([...alphabet]);
+
+/* 
+Output:
+[
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+  'y', 'z'
+]
+*/
